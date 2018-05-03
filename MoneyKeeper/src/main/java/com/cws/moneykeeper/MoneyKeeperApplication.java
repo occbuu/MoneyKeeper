e43d0a5ab3763fa087fcbdb7deee6eca7f52a565
Tri Nguyen <nguyenthtri22095@gmail.com>
2018-05-03 10:33:43 +0700

    Create project SB and upload script for DB.

---
+++ MoneyKeeper/src/main/java/com/cws/moneykeeper/MoneyKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.cws.moneykeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyKeeperApplication.class, args);
+	}
+}
